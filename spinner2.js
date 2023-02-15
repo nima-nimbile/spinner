d463c24c1ce310326dd895807b49c8a26b7379c7
Nima <nima.mohamed@hotmail.com>
2023-02-14 22:30:05 -0800

    W2 D2

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+const movementArr = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"]
+let cunt = 0;
+for (const item of movementArr){
+  cunt += 100;
+  setTimeout(() =>{
+    process.stdout.write(`\r${item}   `);
+  }, cunt)
+};
+
+
